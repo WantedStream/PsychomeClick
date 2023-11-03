97a57bf4d878c2651dec19c3c28d00269bc21f46
WantedStream <48957819+WantedStream@users.noreply.github.com>
2023-11-04 00:05:44 +0200

    e

--- Project/JPsychometric/src/main/java/com/projectclasses/prototypeclasses/Test.java
+++ Project/JPsychometric/src/main/java/com/projectclasses/prototypeclasses/Test.java
@@ -7,5 +7,33 @@ public class Test {
     protected int time;
     protected short points;
 
+    public Test(List<Question> questionList, int time, short points) {
+        this.questionList = questionList;
+        this.time = time;
+        this.points = points;
+    }
 
+    public List<Question> getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(List<Question> questionList) {
+        this.questionList = questionList;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public short getPoints() {
+        return points;
+    }
+
+    public void setPoints(short points) {
+        this.points = points;
+    }
 }
