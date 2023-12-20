6ad4b54609c41e277af954828d8899f2fe5eb5a5
RockstarPlayer <152392231+RockstarPlayer@users.noreply.github.com>
2023-12-20 13:10:54 +0200

    added

--- Project/PsychomeClick/app/src/main/java/com/example/psychomeclick/MainActivity.java
+++ Project/PsychomeClick/app/src/main/java/com/example/psychomeclick/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.psychomeclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.psychomeclick.model.UserProgress;
@@ -16,10 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UserProgress up = new UserProgress("");
-        String[] array = {"Verbal reasoning","Analogies"};
+       // UserProgress up = new UserProgress("");
+       // String[] array = {"Verbal reasoning","Analogies"};
         //System.out.println(up.navigateTo(this,array,true));
-        Map.Entry<String, HashMap>  entry=   new AbstractMap.SimpleEntry<String, HashMap>("exmpleString", new HashMap<>());
-       up.addToPath(this,entry, array);
+       // Map.Entry<String, HashMap>  entry=   new AbstractMap.SimpleEntry<String, HashMap>("exmpleString", new HashMap<>());
+       //up.addToPath(this,entry, array);
+        findViewById(R.id.gologInBtn).setOnClickListener((v) -> {
+            Intent intent = new Intent(this,LogIn.class);
+            startActivity(intent);
+        });
+        findViewById(R.id.gosignUpBtn).setOnClickListener((v) -> {
+            Intent intent = new Intent(this,SignUp.class);
+            startActivity(intent);
+        });
+        findViewById(R.id.goenterNoUserBtn).setOnClickListener((v) -> {
+            //
+        });
+        findViewById(R.id.goaboutBtn).setOnClickListener((v) -> {});
     }
 }
