ef8f33d885505137911fee4b47713465dfa1688a
WantedStream <48957819+WantedStream@users.noreply.github.com>
2024-03-22 00:12:20 +0200

    sss

---
+++ Project/PsychomeClick/app/src/main/java/com/example/psychomeclick/ChatBotActivity.java
@@ -0,0 +1,16 @@
+package com.example.psychomeclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class ChatBotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_bot);
+      //  AIzaSyBOiEJFefMXc9TqHUPoG0845A5tDeZ6B7E
+       // String apiKey = BuildConfig.apiKey;
+
+    }
+}
