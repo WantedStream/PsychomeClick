d1d593fc704af65a4c554d06b24a6b615a379b9e
WantedStream <48957819+WantedStream@users.noreply.github.com>
2024-03-22 14:44:15 +0200

    added chat bot

--- Project/PsychomeClick/app/src/main/java/com/example/psychomeclick/ChatBotActivity.java
+++ Project/PsychomeClick/app/src/main/java/com/example/psychomeclick/ChatBotActivity.java
@@ -2,8 +2,13 @@ package com.example.psychomeclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.example.psychomeclick.model.FirebaseManager;
+import com.example.psychomeclick.views.ChatView;
+import com.example.psychomeclick.views.Message;
 import com.google.ai.client.generativeai.GenerativeModel;
 import com.google.ai.client.generativeai.java.ChatFutures;
 import com.google.ai.client.generativeai.java.GenerativeModelFutures;
@@ -13,12 +18,8 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
-import org.reactivestreams.Publisher;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 public class ChatBotActivity extends AppCompatActivity {
 
@@ -33,40 +34,54 @@ public class ChatBotActivity extends AppCompatActivity {
 
         Content.Builder userContentBuilder = new Content.Builder();
         userContentBuilder.setRole("user");
-        userContentBuilder.addText("Hello, I have 2 dogs in my house.");
+       userContentBuilder.addText("Your name is PsychoBot. You are an ai I use in psychometric app. your role is to provide answers in a chat to help topics related to israeli hebrew psychometric questions. you can talk both in english and hebrew. note that the name of the user talking to you is "+ FirebaseManager.userData.getName()+".");
         Content userContent = userContentBuilder.build();
 
         Content.Builder modelContentBuilder = new Content.Builder();
         modelContentBuilder.setRole("model");
-        modelContentBuilder.addText("Great to meet you. What would you like to know?");
-        Content modelContent = userContentBuilder.build();
+       modelContentBuilder.addText("Hello "+FirebaseManager.userData.getName()+". how may I be of assistance to you?");
+        Content modelContent = modelContentBuilder.build();
 
-        List<Content> history = Arrays.asList(userContent, modelContent);
+       List<Content> history = Arrays.asList(userContent, modelContent);
 
-// Initialize the chat
         ChatFutures chat = model.startChat(history);
 
 // Create a new user message
+
       Content.Builder cb= new Content.Builder();
         cb.setRole("user");
-        cb.addText("How many paws are in my house?");
+        cb.addText("give me an psychometric math question");
         Content userMessage=cb.build();
 // Send the message
-        Executor executor = Executors.newSingleThreadExecutor();
         ListenableFuture<GenerateContentResponse> response = chat.sendMessage(userMessage);
 
         Futures.addCallback(response, new FutureCallback<GenerateContentResponse>() {
             @Override
             public void onSuccess(GenerateContentResponse result) {
                 String resultText = result.getText();
-                System.out.println(resultText);
+                ChatView chatView = findViewById(R.id.chatView);
+
+               // chatView.addMessage(resultText, true, true, Color.BLACK, R.drawable.bg);
+               // chatView.addMessage(resultText, false, false, Color.BLUE, R.drawable.bg2);
+               // chatView.addMessage(resultText, true, true, Color.RED, 0);
+                ((EditText)  findViewById(R.id.msgText)).setText("");
             }
 
             @Override
             public void onFailure(Throwable t) {
                 t.printStackTrace();
+                ChatView chatView = findViewById(R.id.chatView);
+                String resultText = t.getMessage();
+               // chatView.addMessage(resultText, true, true, Color.BLACK, R.drawable.bg);
+                //chatView.addMessage(resultText, false, false, Color.BLUE, R.drawable.bg2);
+               // chatView.addMessage(resultText, true, true, Color.RED, 0);
+                ((EditText)  findViewById(R.id.msgText)).setText("");
             }
-        }, executor);
+        }, getMainExecutor());
+        ChatView chatView = findViewById(R.id.chatView);
+        chatView.addMessage(new Message("Hello", true, true, Color.BLACK, R.drawable.bg));
+        chatView.addMessage(new Message("Hi there", false, false, Color.BLUE, R.drawable.bg5));
+        chatView.addMessage(new Message("How are you?", true, true, Color.RED, 0)); // No image
 
     }
 }
