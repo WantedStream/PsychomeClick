66b369b53f688f73fa06adf8162aa9d525d9bb55
WantedStream <48957819+WantedStream@users.noreply.github.com>
2024-05-15 13:23:41 +0300

    add are you sure to sign out

--- Project/PsychomeClick/app/src/main/java/com/example/psychomeclick/UserActivity.java
+++ Project/PsychomeClick/app/src/main/java/com/example/psychomeclick/UserActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -18,7 +20,6 @@ import com.example.psychomeclick.fragments.StopperFragment;
 import com.example.psychomeclick.model.FirebaseManager;
 import com.example.psychomeclick.fragments.GeneralFragment;
 import com.google.gson.Gson;
-
 import java.util.LinkedHashMap;
 
 public class UserActivity extends AppCompatActivity{
@@ -30,13 +31,27 @@ public class UserActivity extends AppCompatActivity{
         createStuff();
 
         findViewById(R.id.signoutbutton).setOnClickListener((t)->{
-            FirebaseManager.userData=null;
-            FirebaseManager.firebaseAuth.signOut();
-            deleteUserFromShared(this.getSharedPreferences(FirebaseManager.PrefLocaltion,MODE_PRIVATE));
-            Intent intent = new Intent(this,MainActivity.class);
-            startActivity(intent);
-            finish();
+            DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        FirebaseManager.userData=null;
+                        FirebaseManager.firebaseAuth.signOut();
+                        deleteUserFromShared(this.getSharedPreferences(FirebaseManager.PrefLocaltion,MODE_PRIVATE));
+                        Intent intent = new Intent(this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to sign out?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+
         });
+
         findViewById(R.id.aibtn).setOnClickListener((t)->{
             Intent intent = new Intent(this, ChatBotActivity.class);
             startActivity(intent);
